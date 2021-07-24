b73856050ecedbd80b1c739eea9e42b9c935f0c7
EricMiquelis <67070626+EricMiquelis@users.noreply.github.com>
2021-07-24 14:50:41 -0300

    Add files via upload

---
+++ medias.py
@@ -0,0 +1,8 @@
+a = int(input("Digite a 1ª nota: "))
+b = int(input("Digite a 2ª nota: "))
+c = int(input("Digite a 3ª nota: "))
+d = int(input("Digite a 4ª nota: "))
+
+media = (a + b + c + d)/4
+
+print("A média aritmética é:",media)         
